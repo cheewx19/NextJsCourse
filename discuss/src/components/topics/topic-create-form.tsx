756ab6ae37adaf6560be7ffb1cbd1ada34892689
cheewx19 <cheewx@hotmail.com>
2023-12-13 01:06:58 +0800

    update

---
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Textarea,
+} from "@nextui-org/react";
+import * as actions from "@/actions";
+import { useFormState } from "react-dom";
+import FormButton from "../common/form-button";
+
+export default function TopicCreateForm() {
+  // make sure the param object here matches the type returned by createTopic
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action={action}>
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="test-lg">Create a Topic</h3>
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name} // !! is to convert any type to boolean value
+              errorMessage={formState.errors.name?.join(", ")}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description} // !! is to convert any type to boolean value
+              errorMessage={formState.errors.description?.join(", ")}
+            />
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+            <FormButton>Save</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+}
